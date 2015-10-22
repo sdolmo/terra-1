eff720663bf599dbaa5851ec0927244caee578ca
sdolmo <sylviad1221@gmail.com>
2015-10-22 09:59:21 -0400

    devise/username conflict check

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+         :recoverable, :rememberable, :trackable, :validatable
 
   has_many :listings
-  attr_accessor :login
 
-  def self.find_for_database_authentication(warden_conditions)
-    conditions[:email].downcase! if conditions[:email]
-    where(conditions.to_h).first
-  end
+
 end
